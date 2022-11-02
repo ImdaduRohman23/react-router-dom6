749aa3390cba371c3e654b1bae77b510cca42168
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-03 06:22:56 +0700

    reading url params

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Detail = () => {
+    const {id} = useParams();
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(res => res.json())
+        .then(data => setUser(data))
+        .catch(err => console.log(err))
+    }, [id])
+
     return (
         <div>
-            <h1>This is Detail</h1>
+            <h1>This is Detail: {id}</h1>
+            <pre>
+                {JSON.stringify(user, null, 2)}
+            </pre>
         </div>
     )
 }
