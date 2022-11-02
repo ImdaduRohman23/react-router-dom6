f325de96dbda1f5a67b21de69543bb8366ce3d13
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-11-03 00:05:49 +0700

    configuring route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Detail from './pages/Detail';
 
 function App() {
   return (
     <div className="App">
-      <h1>Oii</h1>
+        <BrowserRouter>
+          <Routes>
+            < Route path='/' element={<Home />} />
+            < Route path='/:id' element={<Detail />} />
+            < Route path='/login' element={<Login />} />
+            {/* < Route path='/login' element={<Login />} /> */}
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
